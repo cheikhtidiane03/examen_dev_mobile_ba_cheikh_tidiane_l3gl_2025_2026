bb49e2d630891f51666e9520cced4f2543bbf1fe
Cheikh Tidiane Ba <cheikhtidianeb764@gmail.com>
2026-01-23 18:14:58 +0000

    Implémentation du service de stockage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:sunuTask/services/storage_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await StorageService.instance.init();
+
   runApp(const MyApp());
 }
 
